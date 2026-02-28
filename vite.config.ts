e70ed9e90e61759b205358b5c247c9ccbb30bb4a
SGA <ajas73737@gmail.com>
2026-02-28 21:59:59 +0300

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,22 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
+
+  base: "/Abjd-zoone/",
+
   server: {
-    base: "/Abjd-zoone/",
     host: "::",
     port: 8080,
     hmr: {
       overlay: false,
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
