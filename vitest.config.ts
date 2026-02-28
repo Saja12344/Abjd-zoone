9b065f59db9f118256b32339927790f0ffc84391
SGA <ajas73737@gmail.com>
2026-03-01 02:17:28 +0300

    fix servers problem

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,40 +1,16 @@
-// import { defineConfig } from "vitest/config";
-// import react from "@vitejs/plugin-react-swc";
-// import path from "path";
-
-// export default defineConfig({
-//   plugins: [react()],
-//     build: {
-//     outDir: "docs", // 👈 هنا تخلي مخرجات البناء تروح لمجلد docs
-//     emptyOutDir: true
-//   },
-//   test: {
-//     environment: "jsdom",
-//     globals: true,
-//     setupFiles: ["./src/test/setup.ts"],
-//     include: ["src/**/*.{test,spec}.{ts,tsx}"],
-//   },
-//   resolve: {
-//     alias: { "@": path.resolve(__dirname, "./src") },
-//   },
-// });
-
-
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-
-  build: {
-    outDir: "docs",   // 👈 أهم سطر
-    emptyOutDir: true
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: ["./src/test/setup.ts"],
+    include: ["src/**/*.{test,spec}.{ts,tsx}"],
   },
-
   resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+    alias: { "@": path.resolve(__dirname, "./src") },
   },
 });
