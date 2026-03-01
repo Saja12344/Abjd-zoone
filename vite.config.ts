0c2c75e6df378144ceed56f21d6a4b83af18d08a
SGA <ajas73737@gmail.com>
2026-03-01 18:22:22 +0300

    Vercel build logs

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
+// import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  
   base: "./",
   server: {
     host: "::",
@@ -13,8 +14,11 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
+   build: {
+    outDir: "dist"
+  },
   
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  // plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
