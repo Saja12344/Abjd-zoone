7801ea68c4fbfe217658798a0955532b5ce9b7b7
SGA <ajas73737@gmail.com>
2026-03-09 21:05:41 +0300

    without basename

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
       <Toaster />
       <Sonner />
 
-      <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <BrowserRouter >
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="*" element={<NotFound />} />
--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,8 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   
-   base: '/Abjd-zoone/',
+  //  base: '/Abjd-zoone/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
