c3e9a2a1aa4522e3d1108bbfdc14bc0f540556d2
SGA <ajas73737@gmail.com>
2026-03-09 20:52:35 +0300

    change router

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
       <Toaster />
       <Sonner />
 
-      <BrowserRouter basename="/Abjd-zoone/" >
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="*" element={<NotFound />} />
--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   
-  base: "./",
+   base: '/Abjd-zoone/',
   server: {
     host: "::",
     port: 8080,
